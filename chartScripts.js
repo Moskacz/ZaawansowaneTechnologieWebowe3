50eea046e028bf59f8cb7ef41934ff71848c3bc8
Michał Moskała <moskala.michal@gmail.com>
2014-12-06 19:06:03 +0100

    Plotting..

--- chartScripts.js
+++ chartScripts.js
@@ -1,40 +1,84 @@
+var myChartIndex = 0;
 var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+function getUsername() {
+    var userName = location.search.split('username=')[1];
+    return userName;
+}
 
 function getChartLabels() {
     return ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
 }
 
-function getChartValues() {
-    return [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()];
+function getChartValues(userName) {
+    var parameters = {"userName" : userName};
+    var chartValues;
+    $.ajax({
+        type: "POST",
+        data: parameters,
+        url: "chartDataProvider.php",
+        async: false,
+        success: function(result) {
+            chartValues = JSON.parse(result);
+            if (chartValues.length == 0) {
+                chartValues = [50,50,50,50,50,50,50];
+            }
+        }
+    });
+    return chartValues;
 }
-
 
 function getChartData() {
+    var companionNames = getCompanionNames();
+    var dataSets = [];
+
+    for (var i = 0; i < companionNames.length; i++) {
+        var name = companionNames[i];
+
+        if (name == getUsername())
+            myChartIndex = i;
+
+        dataSets.push({
+            label: name,
+            fillColor : "rgba(220,220,220,0.2)",
+            strokeColor : "rgba(220,0,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            pointHighlightFill : "#fff",
+            pointHighlightStroke : "rgba(220,220,220,1)",
+            data : getChartValues(name)
+        })
+    }
+
     return {
-        labels : getChartLabels(),
-        datasets : [
-            {
-                label: "My First dataset",
-                fillColor : "rgba(220,220,220,0.2)",
-                strokeColor : "rgba(220,0,220,1)",
-                pointColor : "rgba(220,220,220,1)",
-                pointStrokeColor : "#fff",
-                pointHighlightFill : "#fff",
-                pointHighlightStroke : "rgba(220,220,220,1)",
-                data : getChartValues()
-            }
-        ]
+        labels: getChartLabels(),
+        datasets: dataSets
     }
 }
+
+function getCompanionNames() {
+    var companionNames;
+    $.ajax({
+        type: "POST",
+        url: "companionNamesProvider.php",
+        async: false,
+        success: function(result) {
+            companionNames = JSON.parse(result);
+        }
+    });
+    return companionNames;
+}
+
+/* click events */
 
 function didClickOnCanvas(canvas, event) {
     var calculatedPoint = chartPointForCanvasClickEvent(canvas, event);
     var pointIndex = Math.floor(calculatedPoint.x);
-    window.myLine.datasets[0].points[pointIndex].value = calculatedPoint.y;
+    window.myLine.datasets[myChartIndex].points[pointIndex].value = calculatedPoint.y;
     window.myLine.update();
     var points = [];
-    for (var i = 0; i < window.myLine.datasets[0].points.length; i++) {
-        points.push(window.myLine.datasets[0].points[i].value);
+    for (var i = 0; i < window.myLine.datasets[myChartIndex].points.length; i++) {
+        points.push(window.myLine.datasets[myChartIndex].points[i].value);
     }
     saveToDatabase(points)
 }
@@ -57,11 +101,16 @@ function getChartYMax() {
 function saveToDatabase(points) {
     $.ajax({
         url: 'chartDataInserter.php',
-        data: {points: points},
+        data: {points: points, userName: getUsername()},
         type: 'POST'
     });
 }
 
+function updateResults() {
+    
+}
+
+/* point class */
 
 function Point(x, y) {
     this.x = x;

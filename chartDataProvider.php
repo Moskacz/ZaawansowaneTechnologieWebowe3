dcbe488be8ae79fbb229f985066dc848fc525d9a
Michał Moskała <moskala.michal@gmail.com>
2014-12-06 19:05:44 +0100

    Fetching data stored in database.

--- chartDataProvider.php
+++ chartDataProvider.php
@@ -1,4 +1,14 @@
 <?php
-    $username = $_GET['username'];
+    $dbc = mysqli_connect('localhost', 'root', 'root', 'ZaawansowaneTechnologieWebowe3');
+    $username = $_POST['userName'];
+    $query = "SELECT y FROM points WHERE userName = '$username'";
+    $result = mysqli_query($dbc, $query);
 
+    $array = array();
+    while ($data = mysqli_fetch_array($result)) {
+        $array[] = $data['y'];
+    }
+    echo json_encode($array);
+
+    mysqli_close($dbc);
 ?>

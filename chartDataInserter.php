85fcee4035d2003f8d4e62960ff11fba87d1f516
Michał Moskała <moskala.michal@gmail.com>
2014-12-06 19:05:12 +0100

    IF EXISTS MySQL workaround.

--- chartDataInserter.php
+++ chartDataInserter.php
@@ -1,10 +1,17 @@
 <?php
     $dbc = mysqli_connect('localhost', 'root', 'root', 'ZaawansowaneTechnologieWebowe3');
     $points = $_POST['points'];
+    $userName = $_POST['userName'];
     for ($i = 0; $i < count($points); $i++) {
         $xPointCoordinate = $i;
         $yPointCoordinate = $points[$i];
-        $query = "INSERT INTO points (userName, x, y) VALUES('test', $xPointCoordinate, $yPointCoordinate) ON DUPLICATE KEY UPDATE userName=VALUES(userName), x=VALUES(x), y=VALUES(y)";
+
+        $query = "SELECT x FROM points WHERE userName = '$userName' AND x = $xPointCoordinate";
+        if (mysqli_num_rows(mysqli_query($dbc, $query)) != 0) {
+            $query = "UPDATE points SET y = $yPointCoordinate WHERE userName = '$userName' AND x = $xPointCoordinate";
+        } else {
+            $query = "INSERT INTO points(userName, x, y) VALUES('$userName', $xPointCoordinate, $yPointCoordinate)";
+        }
         mysqli_query($dbc, $query) or die("error querying");
     }
     mysqli_close($dbc);
